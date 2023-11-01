ce13054c46bc1715346f61128d78dbc9d93e4bb8
jeremia27 <jeremiasibarani51@gmail.com>
2023-11-01 15:36:17 +0700

    UseHook

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Home() {
 
-  //Event on React
+  //Event on React done
+  //UseHook done
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
 
   const handleClick = (e) => {
-    console.log("Hello Ninjas" + e);
+    setName('luigi');
+    setAge(30);
   }
 
   const handleClickAgain = (name, e) => {
@@ -15,6 +19,7 @@ export default function Home() {
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <p>{name} is {age} years old</p>
       <button onClick={handleClick}>Click me</button>
       <button onClick={(e) => handleClickAgain('ayang', e)}>Click me again</button>
     </div>
